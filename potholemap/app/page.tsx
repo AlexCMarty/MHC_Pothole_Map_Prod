cda53a28995d9cecffcff70336d17df492fcb043
afaynin <surgedile@gmail.com>
2026-03-28 12:57:11 -0400

    only once on open

--- potholemap/app/page.tsx
+++ potholemap/app/page.tsx
@@ -4,9 +4,7 @@ import { RequestGeolocation } from "@/components/RequestGeolocation";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="fixed right-4 top-4 z-10">
-        <RequestGeolocation />
-      </div>
+      <RequestGeolocation requestOnMount />
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
