649119f7020a19dcbae50211d03444c405b64043
Kry12345 <gaweckikrystian789@gmail.com>
2026-03-28 14:03:44 -0400

    Report Form Update

--- potholemap/app/components/reportModal.tsx
+++ potholemap/app/components/reportModal.tsx
@@ -18,29 +18,63 @@ export default function ReportModal() {
       {isOpen && (
         <div className="absolute inset-0 z-[500] bg-black/50 flex items-center justify-center p-4">
           <div className="bg-white rounded-xl shadow-2xl p-6 w-full max-w-md flex flex-col gap-4">
-            <h2 className="text-2xl font-bold text-slate-800">Report a Pothole</h2>
+            {/* This div has been customized by Krystian Gawecki */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-2xl font-bold text-slate-800">Report a Pothole</h2>
+              <button
+                type="button"
+                className="rounded-lg bg-red-600 hover:bg-red-700 text-xl font-bold w-10 h-10"
+                onClick={() => setIsOpen(false)}
+              > X 
+              </button>
+            </div>
             <p className="text-sm text-slate-600">Pinpoint the hazard to warn others.</p>
             
             <form className="flex flex-col gap-3" onSubmit={(e) => { e.preventDefault(); setIsOpen(false); }}>
-              <input 
-                type="text" 
-                placeholder="Location Description (e.g. Right lane on 5th Ave)" 
-                className="w-full border border-slate-300 rounded-lg p-3 text-black focus:ring-2 focus:ring-red-500 outline-none"
-                required
-              />
-              <select className="w-full border border-slate-300 rounded-lg p-3 text-black outline-none">
-                <option value="minor">Minor (Bumpy)</option>
-                <option value="moderate">Moderate (Might pop a tire)</option>
-                <option value="severe">Severe (Crater)</option>
-              </select>
-              <div className="flex gap-3 mt-2">
-                <button 
-                  type="button"
-                  onClick={() => setIsOpen(false)}
-                  className="flex-1 py-3 rounded-lg font-semibold text-slate-700 bg-slate-200 hover:bg-slate-300"
+              <div className="flex flex-col">
+                <p
+                  className="text-slate-800"
+                > Location <span className="text-red-600"> * </span>
+                </p>
+                <input 
+                  type="text" 
+                  placeholder="Location Description (e.g. Right lane on 5th Ave)" 
+                  className="w-full border border-slate-300 rounded-lg p-3 text-black focus:ring-2 focus:ring-red-500 outline-none -mt-0.5"
+                  required
+                />
+              </div>
+              <div className="flex flex-col">
+                <p
+                  className="text-slate-800"
+                > Severity <span className="text-red-600"> * </span>
+                </p>
+                <select 
+                  className="w-full border border-slate-300 rounded-lg p-3 text-black outline-none"
+                  defaultValue="" // sets the initial value
+                  required
                 >
-                  Cancel
-                </button>
+                  <option value="" disabled>
+                    --Select an option--
+                  </option>
+                  <option value="minor">Minor (Bumpy)</option>
+                  <option value="moderate">Moderate (Might pop a tire)</option>
+                  <option value="severe">Severe (Crater)</option>
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <p
+                  className="text-slate-800"
+                > Image(s)
+                </p>
+                <input
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  className="w-full border border-slate-300 rounded-lg p-3 text-black outline-none"
+                ></input>
+              </div>
+              
+              <div className="flex gap-3 mt-2">
                 <button 
                   type="submit"
                   className="flex-1 py-3 rounded-lg font-semibold text-white bg-red-600 hover:bg-red-700"
